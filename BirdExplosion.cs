0a4f9c374996c42610494a777facc124982d8749
joshuapole99 <joshuapole@live.nl>
2017-12-22 09:16:45 +0100

    Add files via upload

---
+++ BirdExplosion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdExplosion : MonoBehaviour {
+    
+    private Collider[] hitColliders;
+    public float blastRadius;
+    public LayerMask explosionLayers;
+    public float explosionPower;
+
+   // void OnCollisionEnter(Collision col)
+   // {
+
+   //     ExplosionWork(col.contacts[0].point);
+   //     Destroy(gameObject);
+   //            
+   // }
+
+
+    public void ExplosionWork(Vector3 explosionPoint)
+    {
+        hitColliders = Physics.OverlapSphere(explosionPoint, blastRadius, explosionLayers);
+
+        foreach(Collider hitCol in hitColliders)
+        {
+            if (hitCol.GetComponent<Rigidbody>()!=null)
+            {
+                hitCol.GetComponent<Rigidbody>().isKinematic = false;
+                hitCol.GetComponent<Rigidbody>().AddExplosionForce(explosionPower,explosionPoint, blastRadius, 1, ForceMode.Impulse);
+            }
+        }
+
+    }
+
+
+
+
+
+    
+
+}
---
+++ FlyEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FlyEffect : MonoBehaviour {
+   
+    public AudioClip myclip;
+    // Use this for initialization
+    void Start () {
+   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // If the left mouse button is released...
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            this.gameObject.AddComponent<AudioSource>();
+            this.GetComponent<AudioSource>().clip = myclip;
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
